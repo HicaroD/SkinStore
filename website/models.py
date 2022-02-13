b078bcbc9287ede5c2af6e2147221e5cf1b8e4b6
HicaroD <hdanrlley1@gmail.com>
2022-02-13 16:30:50 -0300

    Creating Product model to represent the skin

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	category = models.CharField(max_length=10, null=True)
+	name = models.CharField(max_length=40, null=True)
+	quality = models.CharField(max_length=40, null=True)
+	price = models.FloatField()
+	primary_image_url = models.CharField(null=True, max_length=50)	
+	secondary_image_url = models.CharField(null=True, max_length=50)
+
+	def __str__(self):
+		return self.name
