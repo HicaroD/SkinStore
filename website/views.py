b4803e8177748b546908201c10b116fe54a6f538
HicaroD <hdanrlley1@gmail.com>
2022-02-13 16:32:41 -0300

    Rendering template for showing the knifes

--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 def homepage(request):
     return render(request, 'website/base.html')
+
+def knifes(request):
+    knifes = Product.objects.filter(category="Knife")
+    return render(request, 'website/knife.html', {"knifes": knifes})
