384079e9b10c578ab2f21a26047614c8102e9ea0
HicaroD <hdanrlley1@gmail.com>
2022-02-14 16:40:09 -0300

    Adding urls of each skin category

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
         path('', views.homepage, name="homepage"),
-        path('knifes/', views.knifes, name="knifes")
+        path('knifes/', views.knifes, name="knifes"),
+        path('gloves/', views.gloves, name="gloves"),
+        path('pistols/', views.pistols, name="pistols"),
+        path('riflers/', views.riflers, name="riflers"),
+        path('awps/', views.awps, name="awps"),
 ]
