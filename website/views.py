f1a93dcfd3eae5e8e072c2b46960ceaf3a99fedf
HicaroD <hdanrlley1@gmail.com>
2022-02-14 16:41:01 -0300

    Adding views to fetch each skin category from DB

--- website/views.py
+++ website/views.py
@@ -6,4 +6,20 @@ def homepage(request):
 
 def knifes(request):
     knifes = Product.objects.filter(category="Knife")
-    return render(request, 'website/knife.html', {"knifes": knifes})
+    return render(request, 'website/product_list.html', {"weapons": knifes})
+
+def pistols(request):
+    pistols = Product.objects.filter(category="Pistol")
+    return render(request, 'website/product_list.html', {"weapons": pistols})
+
+def awps(request):
+    awps = Product.objects.filter(category="AWP")
+    return render(request, 'website/product_list.html', {"weapons": awps})
+
+def riflers(request):
+    riflers = Product.objects.filter(category="Rifler")
+    return render(request, 'website/product_list.html', {"weapons": riflers})
+
+def gloves(request):
+    gloves = Product.objects.filter(category="Glove")
+    return render(request, 'website/product_list.html', {"weapons": gloves})
