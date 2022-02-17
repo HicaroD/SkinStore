a7d6534e164ced919dab0c6c2edb8e1e76e1ab52
HicaroD <hdanrlley1@gmail.com>
2022-02-17 15:04:45 -0300

    Adding new app `Registration`

--- skinStore/settings.py
+++ skinStore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website.apps.WebsiteConfig",
+    "registration.apps.RegistrationConfig"
 ]
 
 MIDDLEWARE = [
