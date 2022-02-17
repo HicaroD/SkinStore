defc7d692500605f335888cfa3da29176edb1bf6
HicaroD <hdanrlley1@gmail.com>
2022-02-17 15:05:46 -0300

    Fixing indentation

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.homepage, name="homepage"),
-        path('knifes/', views.knifes, name="knifes"),
-        path('gloves/', views.gloves, name="gloves"),
-        path('pistols/', views.pistols, name="pistols"),
-        path('riflers/', views.riflers, name="riflers"),
-        path('awps/', views.awps, name="awps"),
+    path('', views.homepage, name="homepage"),
+    path('knifes/', views.knifes, name="knifes"),
+    path('gloves/', views.gloves, name="gloves"),
+    path('pistols/', views.pistols, name="pistols"),
+    path('riflers/', views.riflers, name="riflers"),
+    path('awps/', views.awps, name="awps"),
 ]
