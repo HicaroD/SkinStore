5bd41598106dbedb11d41a1665eae4b633faed8e
HicaroD <hdanrlley1@gmail.com>
2022-02-20 18:30:16 -0300

    Adding url patterns from login, register and logout

--- registration/urls.py
+++ registration/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('register/', views.register, name="register")
+    path('login/', views.login_user, name="login"),
+    path('register/', views.register_user, name="register"),
+    path('logout/', views.logout_user, name="logout")
 ]
