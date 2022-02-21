c4618c38dad79d226c184242727c04aeeee2657e
HicaroD <hdanrlley1@gmail.com>
2022-02-21 12:25:31 -0300

    Removing `TemplateView` (not necessary)

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView
 from .models import Product
 
 class MainTemplateView(ListView):
