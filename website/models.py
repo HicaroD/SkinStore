6356ce2b8c81864311a57d21823cf812ab06e659
HicaroD <hdanrlley1@gmail.com>
2022-02-21 13:36:17 -0300

    New models: Property, Category and Skin
    `Skin` will contain all the data about the product. `Property` and
    `Category` are models that will act as `ForeignKey` on the model `Skin`

--- website/models.py
+++ website/models.py
@@ -1,15 +1,58 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-class Product(models.Model):
-	category = models.CharField(max_length=10, null=True)
-	name = models.CharField(max_length=40, null=True)
-	quality = models.CharField(max_length=40, null=True)
-	price = models.FloatField()
-	primary_image_url = models.CharField(null=True, max_length=50)	
-	secondary_image_url = models.CharField(null=True, max_length=50)
+class Property(models.Model):
+    class Properties(models.TextChoices):
+        FACTORY_NEW    =  "FN", _("Factory New"),
+        MINIMAL_WEAR   =  "MW", _("Minimal Wear"),
+        FIELD_TESTED   =  "FT", _("Field Tested"),
+        WELL_WORM      =  "WW", _("Well Worm"),
+        BATTLE_SCARRED =  "BS", _("Battle Scarred"),
 
-	def __str__(self):
-		return self.name
+    name = models.CharField(
+        max_length=25,
+        choices=Properties.choices,
+        default=Properties.FACTORY_NEW,
+    )
 
-	class Meta:
-		ordering = ["-price"]
+    def __str__(self):
+        return __str__
+
+    class Meta:
+        db_table = "property"
+
+class Category(models.Model):
+    class Categories(models.TextChoices):
+        GLOVE  = "G", _('Glove'),
+        PISTOL = "P", _('Pistol'),
+        AWP    = "AW", _('AWP'),
+        RIFLER = "R", _('Rifler'),
+        KNIFE  = "K", _('Knife')
+
+    name = models.CharField(
+        max_length=25,
+        choices=Categories.choices,
+        default=Categories.KNIFE,
+    )
+
+    def __str__(self):
+        return __str__
+
+    class Meta:
+        db_table = "category"
+
+
+class Skin(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, null=True)
+    skin_property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=19, decimal_places=4)
+    primary_image_url = models.CharField(null=True, max_length=50)	
+    secondary_image_url = models.CharField(null=True, max_length=50)
+
+    def __str__(self):
+	    return self.name
+
+    class Meta:
+        db_table = "skin"
+        ordering = ["-price"]
