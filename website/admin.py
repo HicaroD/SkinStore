0b1c12c9dcc3d4a29354f4e07be379651e040b1e
HicaroD <hdanrlley1@gmail.com>
2022-02-21 13:35:51 -0300

    Adding new Models to the Django admin server

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Product
+from .models import Skin, Category, Property
 
-# Register your models here.
-
-admin.site.register(Product)
+admin.site.register(Skin)
+admin.site.register(Category)
+admin.site.register(Property)
