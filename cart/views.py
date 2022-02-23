523a4015f34abd2488c1a7a9c16f9c2c4cf5b489
HicaroD <hdanrlley1@gmail.com>
2022-02-23 19:33:17 -0300

    Cart is only avaiable when you're logged

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def cart(request):
+    return render(request, 'cart/cart.html')
