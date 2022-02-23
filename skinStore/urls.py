45e4162ff24ffdfe9858bc45ec3e7aa159ee9a54
HicaroD <hdanrlley1@gmail.com>
2022-02-23 18:45:18 -0300

    Fixing syntax error because of comma

--- skinStore/urls.py
+++ skinStore/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("website.urls")),
-    path("", include("registration.urls"))
+    path("", include("registration.urls")),
     path("cart/", include("cart.urls"))
 ]
