5f43396aa206c72742eff2f0ff1afa87fc191e38
HicaroD <hdanrlley1@gmail.com>
2022-02-23 21:10:27 -0300

    Registering new apps

--- skinStore/settings.py
+++ skinStore/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website.apps.WebsiteConfig",
-    "registration.apps.RegistrationConfig"
+    "registration.apps.RegistrationConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
