1ba4ac7862d626fbf841357aec9a567d1b5fdf7d
HicaroD <hdanrlley1@gmail.com>
2022-02-27 00:28:14 -0300

    Adding url pattern to add product to cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.cart, name="cart")
+    path("", views.cart, name="cart"),
+    path("add/<int:pk>", views.add_to_cart, name="add")
 ]
